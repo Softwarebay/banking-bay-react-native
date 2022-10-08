b8109bea96ac88ce5c12670f4bf2b64ad79b4f5b
Jolanta Ślipko <j.slipko21@gmail.com>
2022-10-08 15:16:51 +0200

    Add colors map

---
+++ src/theme/index.ts
@@ -0,0 +1,9 @@
+export const colors = {
+	background: '#F5f5f5',
+	mainDark: '#1B1D4D',
+	bodyText: '#4C4C60',
+	link: '#FF5887',
+	buttonLight: '#97DAFF',
+	buttonDark: '#16579D',
+	buttonText: '#FFFFFF',
+};
