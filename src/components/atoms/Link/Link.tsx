32e89c92b5746e4254d584a2a0ec1755619027c0
Jolanta Ślipko <j.slipko21@gmail.com>
2022-10-13 17:46:51 +0200

    Add Link component, stories
    Disable jsx/a11y rule, href not needed

---
+++ src/components/atoms/Link/Link.tsx
@@ -0,0 +1,23 @@
+import { Pressable, StyleProp, Text, TextStyle } from 'react-native';
+
+import { styles } from './Link.styles';
+
+interface Props {
+	text: string;
+	onPress: () => void;
+	testID: string;
+	style?: StyleProp<TextStyle>;
+}
+
+export const Link = ({ text, onPress, testID, style }: Props) => {
+	return (
+		<Pressable
+			hitSlop={20}
+			style={({ pressed }) => pressed && styles.pressed}
+			testID={testID}
+			onPress={onPress}
+		>
+			<Text style={[styles.link, style]}>{text}</Text>
+		</Pressable>
+	);
+};
--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -7,6 +7,7 @@ function loadStories() {
 	require('../src/components/atoms/Button/Button.stories');
 	require('../src/components/atoms/Heading/Heading.stories');
 	require('../src/components/atoms/InputField/InputField.stories');
+	require('../src/components/atoms/Link/Link.stories');
 	require('../src/components/atoms/Logo/Logo.stories');
 	require('../src/components/atoms/Paginator/Paginator.stories');
 	require('../src/components/atoms/Text/Text.stories');
@@ -18,6 +19,7 @@ const stories = [
 	'../src/components/atoms/Button/Button.stories',
 	'../src/components/atoms/Heading/Heading.stories',
 	'../src/components/atoms/InputField/InputField.stories',
+	'../src/components/atoms/Link/Link.stories',
 	'../src/components/atoms/Logo/Logo.stories',
 	'../src/components/atoms/Paginator/Paginator.stories',
 	'../src/components/atoms/Text/Text.stories',
