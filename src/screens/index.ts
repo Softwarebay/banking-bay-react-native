49e813739ea7a7ece01b7d742f57a76d392a3075
Jolanta Ślipko <j.slipko21@gmail.com>
2022-10-28 17:26:10 +0200

    Fix folder typo

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -3,7 +3,7 @@ export { SignInScreen } from './SignInScreen/SignInScreen';
 export { DashboardScreen } from './DashboardScreen/DashboardScreen';
 export { DepositsScreen } from './DepositsScreen/DepositsScreen';
 export { LoansScreen } from './LoansScreen/LoansScreen';
-export { NotificationsScreen } from './NotificationScreen/NotificationsScreen';
+export { NotificationsScreen } from './NotificationsScreen/NotificationsScreen';
 export { MoreScreen } from './MoreScreen/MoreScreen';
 export { MobilePaymentScreen } from './MobilePaymentScreen/MobilePaymentScreen';
 export { SuccessPaymentScreen } from './SuccessPaymentScreen/SuccessPaymentScreen';
