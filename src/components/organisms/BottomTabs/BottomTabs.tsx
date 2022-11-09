1d9f1d581b61c048c9d9aa6fde8c51ce54cfac06
Jolanta Ślipko <j.slipko21@gmail.com>
2022-11-09 14:44:39 +0100

    Optimize onPress in BottomTabs

--- src/components/organisms/BottomTabs/BottomTabs.tsx
+++ src/components/organisms/BottomTabs/BottomTabs.tsx
@@ -1,5 +1,7 @@
-import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
+import { useCallback } from 'react';
 import { Pressable } from 'react-native';
+import { Route } from '@react-navigation/native';
+import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
 
 import { Flex, Icon, IconProps, Text } from 'components/atoms';
 import { colors } from 'theme';
@@ -7,36 +9,41 @@ import { colors } from 'theme';
 import { styles } from './BottomTabs.styles';
 
 export const BottomTabs = ({ state, navigation }: BottomTabBarProps) => {
+	const onPress = useCallback(
+		(route: Route<string>, isFocused: boolean) => {
+			const event = navigation.emit({
+				type: 'tabPress',
+				target: route.key,
+				canPreventDefault: true,
+			});
+
+			if (!isFocused && !event.defaultPrevented) {
+				navigation.navigate(route.name);
+			}
+		},
+		[navigation]
+	);
+
+	const onLongPress = useCallback(
+		(route: Route<string>) => {
+			navigation.emit({
+				type: 'tabLongPress',
+				target: route.key,
+			});
+		},
+		[navigation]
+	);
 	return (
 		<Flex direction="row" style={styles.container}>
 			{state.routes.map((route, index) => {
 				const isFocused = state.index === index;
 
-				const onPress = () => {
-					const event = navigation.emit({
-						type: 'tabPress',
-						target: route.key,
-						canPreventDefault: true,
-					});
-
-					if (!isFocused && !event.defaultPrevented) {
-						navigation.navigate(route.name);
-					}
-				};
-
-				const onLongPress = () => {
-					navigation.emit({
-						type: 'tabLongPress',
-						target: route.key,
-					});
-				};
-
 				return (
 					<Pressable
 						key={route.key}
 						style={({ pressed }) => [styles.tabItem, pressed && styles.pressed]}
-						onLongPress={onLongPress}
-						onPress={onPress}
+						onLongPress={() => onLongPress(route)}
+						onPress={() => onPress(route, isFocused)}
 					>
 						<Icon
 							color={isFocused ? colors.pink : colors.bodyText}
