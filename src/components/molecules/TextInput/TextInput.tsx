6d50ec9328eee23ed50eaef1675a1dad044d99a8
Jolanta Ślipko <j.slipko21@gmail.com>
2022-11-27 12:50:02 +0100

    Add TextInput

---
+++ src/components/molecules/TextInput/TextInput.tsx
@@ -0,0 +1,28 @@
+import { forwardRef } from 'react';
+import { StyleProp, TextStyle } from 'react-native';
+
+import { Card, InputField, InputFieldHandle } from 'components/atoms';
+
+import { styles } from './TextInput.styles';
+
+export interface Props {
+	placeholder: string;
+	testID?: string;
+	style?: StyleProp<TextStyle>;
+}
+
+export const TextInput = forwardRef<InputFieldHandle, Props>(
+	({ placeholder, style, testID = 'test-id' }, ref) => {
+		return (
+			<Card style={[styles.container, style]} testID={testID}>
+				<InputField
+					ref={ref}
+					keyboardType="default"
+					placeholder={placeholder}
+					secureTextEntry={false}
+					testID={`input-field-${testID}`}
+				/>
+			</Card>
+		);
+	}
+);
